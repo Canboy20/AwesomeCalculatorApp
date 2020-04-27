23c48b03c34289dd85877a27d3febd1d6819594b
Can <can.atay20@gmail.com>
2020-04-27 12:44:46 +0400

    Initial

---
+++ app/src/main/java/com/test/awesomecalculatorapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.test.awesomecalculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.test.awesomemath.AwesomeCalculator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ awesomemath/src/main/java/com/test/awesomemath/AwesomeCalculator.java
@@ -0,0 +1,25 @@
+package com.test.awesomemath;
+
+public class AwesomeCalculator {
+
+    public static int addTwoAwesomeNumbers(int num1, int num2) {
+
+        return num1 + num2;
+    }
+
+    public static int addThreeAwesomeNumbers(int num1, int num2, int num3) {
+
+        return num1 + num2 + num3;
+    }
+
+    public static int multiplyTwoAwesomeNumbers(int num1, int num2) {
+
+        return num1 * num2;
+    }
+
+    public static int multiplyThreeAwesomeNumbers(int num1, int num2, int num3) {
+
+        return num1 * num2 * num3;
+    }
+
+}
